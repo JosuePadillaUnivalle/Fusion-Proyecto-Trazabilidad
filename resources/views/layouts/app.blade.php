0717c6656a692272b7cb2bf563420eae99266c24
Hamza <hamza.cassis@gmail.com>
2026-05-06 22:49:50 -0400

    fix(layout): add CSRF meta tag for JS requests

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>@yield('title', 'Fusion-Proyectos | Panel')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     {{-- FONT AWESOME (CDN) --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
